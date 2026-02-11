fix gradient

scripts/ai_train.py
@@ -31,10 +31,10 @@ def my_train(dataset, ModelClass, verbose=False):
         threshold_mode='rel' # Relative improvement
     )
 
-    criterion = nn.MSELoss() # Standard loss for regression (measurements)
+    criterion = nn.MSELoss(reduction='sum') # Standard loss for regression (measurements)
     model.train()
     prev_loss = 1
-    for epoch in range(1):
+    for epoch in range(50):
         loop = tqdm(loader, desc=f"Epoch {epoch+1}/50")
         total_loss = 0
         for traj_tuple in loop:

scripts/libmy/libdata.py
@@ -4,10 +4,13 @@ import torch
 
 def norm_data_mean_stddev_len(dataset):
     raw_data_keys = dataset.raw_data.keys()
+    #absolute_importance = 1e3
     for key in raw_data_keys:
         std, mean = torch.std_mean(dataset.raw_data[key], dim=(0,1))
+        total_std = torch.std(dataset.raw_data[key])
+        std[std == 0] = 1/(3*total_std)
         width = dataset.stats[key]["size"][-1]
-        scale = std*width
+        scale = std*width#/absolute_importance
         dataset.raw_data[key] -= mean
         dataset.raw_data[key] /= scale
         dataset.stats[key]["train_offset"] = mean